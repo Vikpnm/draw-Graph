fd30bd116b335a682c3e46bbd65208be9e898b3b
Vikpnm <117345872+Vikpnm@users.noreply.github.com>
2024-03-14 20:28:49 +0200

    Add files via upload

---
+++ V1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Defined numbers for plotting the graph
+x_values = [1, 2, 3, 4, 5]
+y_values = [2, 4, 6, 8, 10]
+
+# Plotting the graph
+plt.plot(x_values, y_values)
+
+# Adding title and axis labels
+plt.title('Example Graph')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+# Displaying the graph
+plt.show()
